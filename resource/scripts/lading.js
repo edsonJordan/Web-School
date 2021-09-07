780d2a38ad8159a437e1e11e427c4057b2171aa2
Edson Jordan <edson_4555@hotmail.com>
2021-09-07 01:40:56 -0500

    ending section results

--- resource/scripts/lading.js
+++ resource/scripts/lading.js
@@ -14,8 +14,6 @@ points?.forEach(point => {point.addEventListener('click', (e)=> {
     }  
   })
 })
-
-
 /* Scroll horizontal Box Card */
 var timeCard = null;
 document.getElementById('boxSlider')?.addEventListener('scroll', (e) => {
@@ -25,6 +23,39 @@ document.getElementById('boxSlider')?.addEventListener('scroll', (e) => {
     const listPoint = document.getElementById('pointCard');
     let posiScroll = document.getElementById('boxSlider').scrollLeft;
     let widthDiv = document.getElementById('boxSlider').getElementsByClassName('slider__card')[0].clientWidth;
+    let focusDiv = posiScroll / widthDiv;
+    let selectPoint = listPoint.getElementsByTagName('label')[Math.round(focusDiv)];
+    listPoint.getElementsByClassName('active')[0].classList.remove('active');
+    selectPoint.classList.add('active')
+  }, 250);
+}, true);
+
+
+/* Events clicks points  */
+const pointsResult = document.querySelectorAll('.point__cardResult');
+pointsResult?.forEach(point => {point.addEventListener('click', (e)=> {
+  if(e.target.nodeName == "LABEL"){      
+      let parentCard = document.getElementById('boxSliderResult');
+      e.target.parentNode.getElementsByClassName('active')[0].classList.remove('active');
+      e.target.classList.add('active');
+      let pointSele= e.target.getAttribute('attr-order');
+      let focusSelet = parentCard.getElementsByClassName('slider__card')[pointSele];
+      parentCard.scroll({
+        left:focusSelet.offsetLeft,
+        behavior: 'smooth'
+      });
+    }  
+  })
+})
+/* Scroll horizontal Box Card */
+var timeCard = null;
+document.getElementById('boxSliderResult')?.addEventListener('scroll', (e) => {
+  clearTimeout(timeCard);
+  //Renew timer
+  timeCard = setTimeout(function () {
+    const listPoint = document.getElementById('pointCardResult');
+    let posiScroll = document.getElementById('boxSliderResult').scrollLeft;
+    let widthDiv = document.getElementById('boxSliderResult').getElementsByClassName('slider__card')[0].clientWidth;
     let focusDiv = posiScroll / widthDiv;
     let selectPoint = listPoint.getElementsByTagName('label')[Math.round(focusDiv)];
     listPoint.getElementsByClassName('active')[0].classList.remove('active');
